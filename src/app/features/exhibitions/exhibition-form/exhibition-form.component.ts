5fee515edd5b78640cb650fe09e55d661d3c3b1f
samuel scarabottolo <samuel.scarabottolo@akera.it>
2026-05-14 17:43:05 +0200

    added select/deselect all books for exhibitions

--- src/app/features/exhibitions/exhibition-form/exhibition-form.component.ts
+++ src/app/features/exhibitions/exhibition-form/exhibition-form.component.ts
@@ -2,6 +2,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -23,6 +24,7 @@ import { LibService } from '../../../core/services/lib.service';
     MatButtonModule,
     MatListModule,
     MatSelectModule,
+    MatCheckboxModule,
     NgFor,
     NgIf,
   ],
@@ -53,6 +55,14 @@ export class ExhibitionFormComponent implements OnInit {
     return !!this.data;
   }
 
+  get allSelected(): boolean {
+    return this.booksForLib.length > 0 && this.selectBookIds.length === this.booksForLib.length;
+  }
+
+  get partiallySelected(): boolean {
+    return this.selectBookIds.length > 0 && this.selectBookIds.length < this.booksForLib.length;
+  }
+
   ngOnInit(): void {
     this.libService.getAll().subscribe((libs) => {
       this.libs = libs;
@@ -68,6 +78,14 @@ export class ExhibitionFormComponent implements OnInit {
       this.booksForLib = lib?.books ?? [];
       this.selectBookIds = [];
     });
+  }
+
+  selectAll(): void {
+    this.selectBookIds = this.booksForLib.map((b) => b.id);
+  }
+
+  deselectAll(): void {
+    this.selectBookIds = [];
   }
 
   toggleBook(bookId: number): void {
